65f8898f14aa92bd2bc02fc18febd112f2243361
Sahil Singh <sahils091@gmail.com>
2020-09-24 14:11:41 -0400

    Initial clean up done/ set up the components required.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoInput from './components/TodoInput';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello From App!
+      <TodoInput/>
+      <TodoItem/>
+      
     </div>
   );
 }
